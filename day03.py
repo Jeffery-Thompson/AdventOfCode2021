9c9013f093b4abe25b89269c9ac54b5c4b825095
Jeff Thompson <jathomps@umn.edu>
2021-12-03 16:53:48 -0600

    first commit

---
+++ day03.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# advent of code day 3, problem 1 & 2
+
+# import pandas as pd
+import numpy as np
+from numpy.lib.shape_base import column_stack
+from scipy import stats
+
+# def split(word):
+#     '''Split a word into letters'''
+    
+#     return(list(word))
+test = np.asarray(['00100', '11110','10110', '10111',
+        '10101', '01111', '00111', '11100',
+        '10000', '11001', '00010', '01010'], dtype='str')
+
+def word_to_ints(word):
+    '''Split a word into letters'''
+    word = list(word)
+    int_list = [int(c) for c in word]
+    return(int_list)
+
+def array_to_decimal(array):
+    '''Convert np array of 0,1s to '''
+        # from stack overflow :/ 
+    return (array.dot(2**np.arange(array.size)[::-1]) )
+
+def submarine_power(data):
+    '''Function to calculate subs power'''
+    data = [word_to_ints(d) for d in data]
+
+    gamma = stats.mode(data)[0].flatten()
+    epsilon = (~gamma.astype('bool')).astype('int')
+
+    # from stack exhange :/
+    gamma = array_to_decimal(gamma)
+    epsilon = array_to_decimal(epsilon)
+
+    return (gamma * epsilon)
+
+data = list(np.loadtxt('./data/day03.txt', str))
+
+power = submarine_power(data)
+
+print('power is: {}'.format(power))
